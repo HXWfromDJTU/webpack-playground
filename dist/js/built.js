4407072901741615dcf98e1273fd3fe982880ca4
swainwong <404813471@qq.com>
2020-11-20 11:46:35 +0800

    chore: CSS处理

---
+++ dist/js/built.js
@@ -0,0 +1,212 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/style/index.scss":
+/*!******************************!*\
+  !*** ./src/style/index.scss ***!
+  \******************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack-playground/./src/style/index.scss?");
+
+/***/ }),
+
+/***/ "./static/font/iconfont.css":
+/*!**********************************!*\
+  !*** ./static/font/iconfont.css ***!
+  \**********************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack-playground/./static/font/iconfont.css?");
+
+/***/ }),
+
+/***/ "./static/imgs/man.png":
+/*!*****************************!*\
+  !*** ./static/imgs/man.png ***!
+  \*****************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements: module, __webpack_require__.p, __webpack_require__.* */
+/*! CommonJS bailout: module.exports is used directly at 1:0-14 */
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"imgs/f0d8c1ce9d.png\";\n\n//# sourceURL=webpack://webpack-playground/./static/imgs/man.png?");
+
+/***/ }),
+
+/***/ "./static/imgs/nexttick_cover.png":
+/*!****************************************!*\
+  !*** ./static/imgs/nexttick_cover.png ***!
+  \****************************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements: module, __webpack_require__.p, __webpack_require__.* */
+/*! CommonJS bailout: module.exports is used directly at 1:0-14 */
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"imgs/ca8e5468f0.png\";\n\n//# sourceURL=webpack://webpack-playground/./static/imgs/nexttick_cover.png?");
+
+/***/ }),
+
+/***/ "./static/imgs/woman.jpeg":
+/*!********************************!*\
+  !*** ./static/imgs/woman.jpeg ***!
+  \********************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements: module, __webpack_require__.p, __webpack_require__.* */
+/*! CommonJS bailout: module.exports is used directly at 1:0-14 */
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"imgs/9197d8ca74.jpeg\";\n\n//# sourceURL=webpack://webpack-playground/./static/imgs/woman.jpeg?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _static_data_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../static/data.json */ \"./static/data.json\");\n/* harmony import */ var _static_font_iconfont_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../static/font/iconfont.css */ \"./static/font/iconfont.css\");\n/* harmony import */ var _static_imgs_nexttick_cover_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../static/imgs/nexttick_cover.png */ \"./static/imgs/nexttick_cover.png\");\n/* harmony import */ var _static_imgs_nexttick_cover_png__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_static_imgs_nexttick_cover_png__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _static_imgs_woman_jpeg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../static/imgs/woman.jpeg */ \"./static/imgs/woman.jpeg\");\n/* harmony import */ var _static_imgs_woman_jpeg__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_static_imgs_woman_jpeg__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _static_imgs_man_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../static/imgs/man.png */ \"./static/imgs/man.png\");\n/* harmony import */ var _static_imgs_man_png__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_static_imgs_man_png__WEBPACK_IMPORTED_MODULE_4__);\n/* harmony import */ var _style_index_scss__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./style/index.scss */ \"./src/style/index.scss\");\n\n\n\n// 字体样式入口\n\n\n// 图片文件打包\n\n\n\n\n// 样式文件包\n\n\nconsole.log(_static_data_json__WEBPACK_IMPORTED_MODULE_0__)\n\n//# sourceURL=webpack://webpack-playground/./src/index.js?");
+
+/***/ }),
+
+/***/ "./static/data.json":
+/*!**************************!*\
+  !*** ./static/data.json ***!
+  \**************************/
+/*! default exports */
+/*! export test [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: module */
+/***/ ((module) => {
+
+"use strict";
+eval("module.exports = {\"test\":\"123123\"};\n\n//# sourceURL=webpack://webpack-playground/./static/data.json?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => module['default'] :
+/******/ 				() => module;
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl + "../";
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 /**
  * webpacl.config.js
  * webpack配置文件默认使用 commonjs
+ * 
+ * mode {
+ *    development: ①着重于本地开发
+ *    production: ①css从JavaScript中提取出来  ②代码压缩   ③CSS语法的浏览器兼容  ④更快更强
+ * }
  */
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractplugin  = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+process.env.NODE_ENV = 'development'
 
 
 module.exports = {
@@ -15,10 +24,9 @@ module.exports = {
 
 
     output: {
-        filename: 'built.js',
+        filename: 'js/built.js',
         // '__dirname' 表示当前目录的绝对路径
         path: path.resolve(__dirname, 'dist'),
-        // publicPath: '/',
     },
     // 配置loader 记性功能拓展
     module: {
@@ -26,10 +34,28 @@ module.exports = {
             test: /\.(scss|css)$/,
             // use 中的执行顺序是从后往前
             use: [
-                'style-loader', // 创建 style 标签 将 js 中的样式资源进行插入，添加到headzhong 
+                // 'style-loader', // 创建 style 标签 将 js 中的样式资源进行插入，添加到 HTML 文件的 head标签中 
+                {
+                    loader: MiniCssExtractplugin.loader, // 使用 MiniCssExtractplugin 的 loader 提取js中的css文件成单独文件
+                    options: {
+                        publicPath: path.resolve(__dirname, 'dist')
+                    }
+                },
                 'css-loader', // 将 css文件中的内容变为字符串， 并且用 commonjs 模块包裹住
-                'sass-loader', // 若不及啊这个laoder也可以打包成功，但是不会按照预期处理了 sass 语法
-            ]
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                require('postcss-preset-env')() // 规则默认读取 package.json 下的 browserList 配置项。分别配置了开发与正式两种环境
+                            ]
+                        }
+                    }
+                },
+                'sass-loader', // 若不加这个laoder也可以打包成功，但是不会按照预期处理了 sass 语法
+            ],
+            // 此处不需要进行 style 文件输出目录的配置，因为都已经打包到了 JavaScript 文件中了
         },
         {
             test: /\.(png|jpe?g|gif)$/,
@@ -37,25 +63,46 @@ module.exports = {
             options: {
                 limit: 2 * 1024, // 限制图片大小小于 2kb, 若大于此值则被处理为 base64编码
                 esModule: false, // url laoder默认使用 esModuel 进行模块化，而搭配的 html loader 是commonjs, 统一配置进而匹配
-                name: '[hash:10].[ext]' // 设置文件的格式 [hash:10] 表示出现10位的随机字符串 [ext]表示拓展名
+                name: '[hash:10].[ext]', // 设置文件的格式 [hash:10] 表示出现10位的随机字符串 [ext]表示拓展名
+                outputPath: 'imgs'
             }
         },
         {
             exclude: /\.(css|scss|js|html|png|jpe?g|gif)/, // 排除已经处理过的资源，剩下的资源原封不动迁移过去
-            loader: 'file-loader'
+            loader: 'file-loader',
+            options: {
+                outputPath: 'media'
+            }
         },
         // {
         //     test: /\.html$/i,
         //     loader: 'html-loader', // 处理 html 文件中的es6模块化，使用commonjs进行解析
+        //     options: {
+        //         minimize: true
+        //     }
         // }
       ]
     },
     // plugin 是拓展功能，是额外的模块，需要下载后引入再进行使用
     // 对于每一个插件 option 是通用配置 参考文档 https://github.com/jantimon/html-webpack-plugin#options
     plugins: [
+        // 自动生成html模板
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'index.html')
         }),
-        new CleanWebpackPlugin()
-    ]
+        new MiniCssExtractplugin({
+            filename: 'css/built.css', // 对输出文件的重命名
+        }),
+        new OptimizeCssAssetsWebpackPlugin(), // 压缩 CSS 文件
+        new CleanWebpackPlugin(), // 用于在下一次打包时清除之前打包的文件
+    ],
+    // 开发服务器 devServer 用来运行自动化
+    // 特点: 只会在内存中编译打包，不会有任何的输出
+    devServer : {
+        contentBase: path.resolve(__dirname, 'dist'),
+        // 启动 gzip
+        compress: true,
+        port: 3001,
+        open: true
+    }
 }
